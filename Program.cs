6122e55380370fe2c46b9277014c30364704f788
Henrik-Ehde <henrik_ehde@hotmail.com>
2024-02-22 13:36:20 +0100

    Added welcome message

--- Program.cs
+++ Program.cs
@@ -7,10 +7,34 @@ using Task = TaskMaster.Task;
 
 FileHandler.Open();
 Menus.Setup();
+ShowWelcomeMessage();
 
 while (true)
 {
     Menus.MainMenu.Open();
+}
+
+void ShowWelcomeMessage()
+{
+    //Display an opening message when program is started. Count and show the number of tasks that are completed, not completed or overdue.
+    int completedCount = 0;
+    int uncompletedCount = 0;
+    int overdueCount = 0;
+
+    foreach (Task task in TaskList.tasks)
+    {
+        if (task.Completed) completedCount++;
+        else
+        {
+            uncompletedCount++;
+            if (task.DueDate <  DateTime.Now) overdueCount++;
+        }
+    }
+
+    Console.WriteLine("Welcome to TaskMaster! You have " + completedCount + " completed tasks and " + uncompletedCount + " uncompleted tasks.");
+
+    if (overdueCount > 0) ColoredText.WriteLine(overdueCount + " tasks are overdue!", ConsoleColor.DarkYellow);
+    Console.Write("\n");
 }
 
 
@@ -50,7 +74,3 @@ void MainMenuOLD()
         while (repeat);
     }
 }
-
-
-
-
